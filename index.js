cad3adbc20dd0579196c7b48034df951102659db
Khaelo <luca.bruno@ynov.com>
2019-03-01 13:55:34 +0100

    Add files for G0RT

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.login("NTUwOTkwMzM1MzQ2NzM3MTcy.D1qfYw.AvqFrfg-qaFskIutGGj9SqAMNdw");
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (error, f) => {
+    if(error) console.log(error);
+
+    let commandes = f.filter(f => f.split(".").pop() === "js");
+    if(commandes.length <= 0) return console.log("Aucune commande trouvée !");
+
+    commandes.forEach((f) => {
+        let commande = require(`./commands/${f}`);
+        console.log(`${f} commande chargée !`);
+
+        client.commands.set(commande.help.name, commande);
+    });
+});
+
+fs.readdir("./events/", (error, f) => {
+    if(error) console.log(error);
+    console.log(`${f.length} events chargés`);
+
+    f.forEach((f) => {
+        const events = require(`./events/${f}`);
+        const event = f.split(".")[0];
+
+        client.on(event, events.bind(null, client));
+    });
+});
